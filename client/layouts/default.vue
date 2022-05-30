4c08887eb20a34065e21ac49fd73073f0d9f156d
nzivo <johnnnzivo@gmail.com>
2022-05-30 17:00:45 +0300

    Default Layout updated

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,6 +1,13 @@
 <template lang="">
   <div>
-    <Nuxt />
+    <v-app>
+      <v-main>
+        <v-app-bar color="green" elevation="4"></v-app-bar>
+        <client-only>
+          <Nuxt />
+        </client-only>
+      </v-main>
+    </v-app>
   </div>
 </template>
 <script>
