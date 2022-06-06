f33fc789a0e0e9a962f7fad276d8050279986aad
nzivo <johnnnzivo@gmail.com>
2022-06-06 11:17:00 +0300

    Added an index to primary key column

--- server/models/user.py
+++ server/models/user.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, Table
 from sqlalchemy.sql.sqltypes import Integer, String
 from config.db import meta, engine
 users = Table('users', meta,
-    Column('id', Integer, primary_key = True),
+    Column('id', Integer, primary_key = True, index=True),
     Column('name', String(255)),
     Column('email', String(255)),
     Column('password', String(255))
